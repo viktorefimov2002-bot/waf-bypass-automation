145bbfa0dae687bc78700e14b1fbba932ec3ce68
v1kt0r_fmv <viktorefimov2002@gmail.com>
2026-07-24 17:40:24 +0300

    Load reviewed extra primitive signatures

--- waf_automation/__init__.py
+++ waf_automation/__init__.py
@@ -2,3 +2,9 @@
 
 __version__ = "0.1.0"
 
+# Keep the main rule generator compact while allowing reviewed primitive packs
+# to extend its signature library during package initialization.
+from . import rules as _rules
+from .extra_primitives import apply_extra_primitives as _apply_extra_primitives
+
+_apply_extra_primitives(_rules.SIGNATURES)
